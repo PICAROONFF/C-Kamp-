25c4035c7a0c6a90e602891b413c196c4821cfa9
ahmet <ahmet@MONSTER>
2024-12-28 17:32:26 +0300

    Proje dosyası ekle.

---
+++ C#1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Değişkenler
+
+            #region string
+
+            //string 
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Selim";
+            //customerSurname = "Tanrıkulu";
+            //customerPhone = "+905555555555";
+            //customerEmail = "ast@hotmail.com";
+            //district = "Merkez";
+            //city = "Isparta";
+
+            //Console.WriteLine("***** Rezervasyon Kartı *****");
+            //Console.WriteLine("------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email: " + customerEmail);
+            //Console.WriteLine("Adres: " + city + "/" + district);
+            //Console.WriteLine("------------------------------");
+
+
+            //customerName = "Ayşe";
+            //customerSurname = "Kaya";
+            //customerPhone = "+905671469898";
+            //customerEmail = "ak@hotmail.com";
+            //district = "Galata";
+            //city = "İstanbul";
+
+            //Console.WriteLine();
+            //Console.WriteLine("------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email: " + customerEmail);
+            //Console.WriteLine("Adres: " + city + "/" + district);
+            //Console.WriteLine("------------------------------");
+
+
+
+
+            #endregion
+
+            #region int
+
+            //int 
+
+            //int number = 32;
+            //Console.WriteLine(number);
+
+            //int hamburger = 300;
+            //int coke = 35;
+            //int water = 10;
+            //int frice = 50;
+            //int pizza = 250;
+            //int lemonade = 30;
+
+            //Console.WriteLine("***** Menü ******");
+            //Console.WriteLine();
+            //Console.WriteLine("---Hamburger: " + hamburger + " $");
+            //Console.WriteLine("---Pizza: " + pizza + " $");
+            //Console.WriteLine("---Frice: " + frice + " $");
+            //Console.WriteLine("---Coke: " + coke + " $");
+            //Console.WriteLine("---Lemonade: " + lemonade + "  $");
+            //Console.WriteLine("---Water: " + water + " $");
+
+            //int hamburgercount;
+            //int cokecount;
+            //int watercount;
+            //int fricecount;
+            //int pizzacount;
+            //int lemonadecount;
+
+            //int totalPrice = 0;
+
+            //hamburgercount = 3;
+            //cokecount = 3;
+            //watercount = 3;
+            //fricecount = 1;
+            //pizzacount = 0;
+            //lemonadecount = 0;
+
+            //totalPrice = hamburger * hamburgercount + coke * cokecount + water * watercount + frice * fricecount + pizza * pizzacount + lemonade * lemonadecount;
+
+            //Console.WriteLine("-------------------------------");
+            //Console.WriteLine("Hesap: " + totalPrice + " $");
+
+
+
+            #endregion
+
+            #region 
+
+
+
+
+
+            #endregion
+
+
+
+
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("C#2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("C#2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("5caaeac9-ce70-48f2-9d4d-0324528d517c")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
